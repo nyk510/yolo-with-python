90291159b626ed6a25f3ea092452810944fb8a81
nyker510 <takamail53@gmail.com>
2018-10-24 18:27:02 +0900

    [add] 入力動画の人物を検知して可視化するスクリプト

---
+++ sample/movie_person_detection.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+
+import cv2
+import numpy as np
+
+from nyktk.cv2_utils import AbstractVideoStream, draw_bbox, BoundingBox
+from nyktk.detect import CV2YOLODetector
+
+
+class SimplePersonDetectVideoStream(AbstractVideoStream):
+    def __init__(self, video_path):
+        self.detector = CV2YOLODetector()
+        self.output_dir = '/home/sample/output_frames'
+        super(SimplePersonDetectVideoStream, self).__init__(video_path)
+
+    def before_read(self):
+        os.makedirs(self.output_dir, exist_ok=True)
+
+    def update(self, frame, frame_counter):
+        detections = self.detector.predict(frame)
+
+        self.logger.info('detect num:{}'.format(len(detections)))
+        draw_img = np.copy(frame)
+        for _, prob, area in detections:
+            bbox = BoundingBox(area)
+            draw_img = draw_bbox(draw_img, bbox)
+
+        out_path = os.path.join(self.output_dir, 'frame_{:05d}.png'.format(frame_counter))
+        cv2.imwrite(out_path, draw_img)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser('movie person detection', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-f', '--file', required=True, type=str)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    stream = SimplePersonDetectVideoStream(video_path=args.file)
+    stream.run()
+
+
+if __name__ == '__main__':
+    main()
