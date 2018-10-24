6517ff10e368ccb7f31e7eaea87932af254f7ddf
nyker510 <takamail53@gmail.com>
2018-10-24 18:32:39 +0900

    [update] 解析する画像を resize してから実行するように変更

--- sample/movie_person_detection.py
+++ sample/movie_person_detection.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import cv2
+import imutils
 import numpy as np
 
 from nyktk.cv2_utils import AbstractVideoStream, draw_bbox, BoundingBox
@@ -18,15 +19,16 @@ class SimplePersonDetectVideoStream(AbstractVideoStream):
         os.makedirs(self.output_dir, exist_ok=True)
 
     def update(self, frame, frame_counter):
+        frame = imutils.resize(frame, width=400)
         detections = self.detector.predict(frame)
 
-        self.logger.info('detect num:{}'.format(len(detections)))
+        self.logger.debug('detect num:{}'.format(len(detections)))
         draw_img = np.copy(frame)
         for _, prob, area in detections:
             bbox = BoundingBox(area)
             draw_img = draw_bbox(draw_img, bbox)
-
         out_path = os.path.join(self.output_dir, 'frame_{:05d}.png'.format(frame_counter))
+        self.logger.info('save to {}'.format(out_path))
         cv2.imwrite(out_path, draw_img)
 
 
