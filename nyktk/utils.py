b62a00dcc1e613611d137374ad225c54de1d3fa4
nyker510 <takamail53@gmail.com>
2018-10-24 17:07:10 +0900

    [bugfix] 保存先が固定になっていたバグの修正

--- nyktk/utils.py
+++ nyktk/utils.py
@@ -5,9 +5,8 @@ utility functions
 import math
 import os
 from logging import getLogger, StreamHandler, FileHandler, Formatter
-from PIL import Image
+
 import requests
-from scipy import spatial
 from tqdm import tqdm
 
 
@@ -47,6 +46,7 @@ def get_logger(name, log_level="DEBUG", output_file=None, handler_level="INFO"):
 
     return logger
 
+
 logger = get_logger(__name__)
 
 
@@ -70,7 +70,7 @@ def download_file(url, save_to):
     total_size = int(r.headers.get('content-length', 0))
     block_size = 1024
     wrote = 0
-    with open('output.bin', 'wb') as f:
+    with open(save_to, 'wb') as f:
         for data in tqdm(r.iter_content(block_size), total=math.ceil(total_size // block_size), unit='KB',
                          unit_scale=True):
             wrote = wrote + len(data)
